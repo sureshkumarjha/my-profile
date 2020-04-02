b00915dfc47b3dac1db6a91296385fbc9de38f11
sureshkumarjha <sureshjha94@gmail.com>
2020-04-02 17:15:28 +0530

    Updates

---
+++ precache-manifest.0d6cf9dd9054cfae2525a4b5509f10b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc34939955d0cc22514615af77f142ed",
+    "url": "/my-profile/index.html"
+  },
+  {
+    "revision": "a2ab2fb670365f003583",
+    "url": "/my-profile/static/css/2.07548e1c.chunk.css"
+  },
+  {
+    "revision": "462320ecd74b8a1b8225",
+    "url": "/my-profile/static/css/main.9b0762f8.chunk.css"
+  },
+  {
+    "revision": "a2ab2fb670365f003583",
+    "url": "/my-profile/static/js/2.529a65c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-profile/static/js/2.529a65c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "462320ecd74b8a1b8225",
+    "url": "/my-profile/static/js/main.27121dab.chunk.js"
+  },
+  {
+    "revision": "dad1b98b7ea6910746ae",
+    "url": "/my-profile/static/js/runtime-main.14169a66.js"
+  }
+]);
