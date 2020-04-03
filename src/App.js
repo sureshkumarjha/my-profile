63739283b5ca57f63f73e935891b4c59c0a93954
sureshkumarjha <sureshjha94@gmail.com>
2020-04-03 14:51:43 +0530

    Sub heading changed

--- src/App.js
+++ src/App.js
@@ -109,7 +109,7 @@ function App() {
     <article className="mw5-ns  center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
       <div className="tc">
         <h1 className="f4">Mimi Whitehouse</h1>
-        <hr className="mw3 bb bw1 b--black-10"/>
+        <hr className="bb bw1 b--black-10"/>
       </div>
       <p className="lh-copy measure center f6 black-70">
         Quite affectionate and outgoing.
@@ -121,7 +121,7 @@ function App() {
     <article className="mw5-ns center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
       <div className="tc">
         <h1 className="f4">Mimi Whitehouse</h1>
-        <hr className="mw3 bb bw1 b--black-10"/>
+        <hr className=" bb bw1 b--black-10"/>
       </div>
       <p className="lh-copy measure center f6 black-70">
         Quite affectionate and outgoing.
@@ -133,7 +133,7 @@ function App() {
     <article className="mw5-ns center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
       <div className="tc">
         <h1 className="f4">Mimi Whitehouse</h1>
-        <hr className="mw3 bb bw1 b--black-10"/>
+        <hr className=" bb bw1 b--black-10"/>
       </div>
       <p className="lh-copy measure center f6 black-70">
         Quite affectionate and outgoing.
@@ -146,7 +146,7 @@ function App() {
       <div className="tc">
       
         <h1 className="f4">Mimi Whitehouse</h1>
-        <hr className="mw3 bb bw1 b--black-10"/>
+        <hr className=" bb bw1 b--black-10"/>
       </div>
       <p className="lh-copy measure center f6 black-70">
         Quite affectionate and outgoing.
@@ -158,7 +158,7 @@ function App() {
     <article className="mw5-ns  center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
       <div className="tc">
         <h1 className="f4">Mimi Whitehouse</h1>
-        <hr className="mw3 bb bw1 b--black-10"/>
+        <hr className=" bb bw1 b--black-10"/>
       </div>
       <p className="lh-copy measure center f6 black-70">
         Quite affectionate and outgoing.
@@ -170,7 +170,7 @@ function App() {
     <article className="mw5-ns center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
       <div className="tc">
         <h1 className="f4">Mimi Whitehouse</h1>
-        <hr className="mw3 bb bw1 b--black-10"/>
+        <hr className=" bb bw1 b--black-10"/>
       </div>
       <p className="lh-copy measure center f6 black-70">
         Quite affectionate and outgoing.
@@ -182,7 +182,7 @@ function App() {
     <article className="mw5-ns center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
       <div className="tc">
         <h1 className="f4">Mimi Whitehouse</h1>
-        <hr className="mw3 bb bw1 b--black-10"/>
+        <hr className=" bb bw1 b--black-10"/>
       </div>
       <p className="lh-copy measure center f6 black-70">
         Quite affectionate and outgoing.
@@ -195,7 +195,7 @@ function App() {
       <div className="tc">
       
         <h1 className="f4">Mimi Whitehouse</h1>
-        <hr className="mw3 bb bw1 b--black-10"/>
+        <hr className=" bb bw1 b--black-10"/>
       </div>
       <p className="lh-copy measure center f6 black-70">
         Quite affectionate and outgoing.
